a2a976be146ca5f8882295099ed4023100c73aad
megadevice83 <megadevice83@gmail.com>
2017-11-26 11:56:26 +0200

    test session - initial commit

---
+++ testSessions/question.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oshevchuk
+ * Date: 26.11.2017
+ * Time: 11:42
+ */
+
+class Question{
+    public $title;
+    public $questions = array();
+    public $correct_answer=array();
+
+    function __construct()
+    {
+        
+    }
+}
---
+++ testSessions/questions/quest1.php
@@ -0,0 +1,10 @@
+<?php
+  $question = [
+
+];
+?>
+<h2>What is number?</h2>
+<div>
+    <input type="checkbox" name="ans" value="" id="q1">
+    <label for="q1">Hello World</label>
+</div>
